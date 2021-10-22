9a820df3de48fe1f4bd578d4883e08154f64504b
maxuyan <201376880@qq.com>
2021-10-22 22:08:08 +0800

    hw5

--- app.rb
+++ app.rb
@@ -56,12 +56,12 @@ class WordGuesserApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
-    # if check_win_or_lose == :win
-    #   redirect '/win'
-    # end
-    # if check_win_or_lose == :lose
-    #   redirect '/lose'
-    # end
+    if WordGuesserGame.check_win_or_lose == :win
+      redirect '/win'
+    end
+    if WordGuesserGame.check_win_or_lose == :lose
+      redirect '/lose'
+    end
     erb :show # You may change/remove this line
   end
   
